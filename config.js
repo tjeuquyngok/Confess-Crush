562f8c6f053d5a6f3d3d40fe5c5638c6b662978e
tjeuquyngok <107044127+tjeuquyngok@users.noreply.github.com>
2022-06-07 18:57:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰',
     messDesc: 'Tối mai 8h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/messages/t/100027035565964'
 }
